541cec16403b1e44310c37e23f351ef1a5622f78
Saint-Jean Germila <germila@noukod.com>
2018-10-02 15:54:24 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,11 +9,11 @@ suite_a: "Amanda Presley",
  def select_winner(passengers)
    # add the code snippet here!
    winner = ""
- 
+
    passengers.each do |suite, name|
      if suite == :suite_a && name.start_with?("A")
        winner = name
    end
- 
+
    winner
  end
